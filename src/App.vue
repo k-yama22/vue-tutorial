4082aa73856f47a88c48396d43f81102cd07c864
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-11-26 22:59:57 +0900

    ✨ UserInfoコンポーネントを追加

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Vue Tutorial" name="k-yama"/>
+  <UserInfo name="山田 太郎" age=25 />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import UserInfo from './components/UserInfo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    UserInfo
   }
 }
 </script>
---
+++ src/components/UserInfo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="user">
+    <h1>ユーザ情報</h1>
+    <div class="name">{{ name }}</div>
+    <div>{{ age }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserInfo',
+  props: {
+    name: String,
+    age: Number
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.name {
+  color: #42b983;
+}
+</style>
