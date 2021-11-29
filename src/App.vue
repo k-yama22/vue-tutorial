ed6b8163710af5bd41a1e4a6a4143d6d8fa7cd8b
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-11-29 23:56:47 +0900

    ✨ axiosを用いたnestjs側からユーザー情報を取得する処理を追加

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Vue Tutorial" name="k-yama"/>
-  <UserInfo name="山田 太郎" age=25 />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Vue Tutorial" name="k-yama" />
+  <UserInfo name="山田 太郎" age="25" />
+  <GetUsers />
+
   <!-- <Button buttonText="テストボタン" /> -->
   <div>
     <button @click="count++">increment</button>
     <button @click="count--">decrement</button>
     <p>{{ count }}</p>
   </div>
+  <div>
+    <div v-for="user in users" :key="user.id">
+      <p>{{ user.name }}</p>
+      <p>{{ user.age }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import GetUsers from "./components/GetUsers.vue";
 // import Button from './components/Button.vue'
-import HelloWorld from './components/HelloWorld.vue'
-import UserInfo from './components/UserInfo.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import UserInfo from "./components/UserInfo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
     UserInfo,
+    GetUsers,
     // Button
   },
   data() {
@@ -33,7 +43,7 @@ export default {
       this.count--;
     },
   },
-}
+};
 </script>
 
 <style>
---
+++ src/components/GetUsers.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>ユーザー情報をaxiosで取得</div>
+    <div v-for="user in users" :key="user.id">
+      <p>{{ user.name }}</p>
+      <p>{{ user.age }}</p>
+    </div>
+    <br />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "GetUsers",
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    this.getUsers();
+  },
+  methods: {
+    getUsers: function () {
+      axios
+        .get("http://localhost:3000/users")
+        .then((res) => {
+          console.log(res);
+          this.users = res.data.users;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
